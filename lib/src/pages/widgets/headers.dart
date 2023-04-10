6ac1f91d358791f8766a2d3b07eb2ae6a70fc48e
cesarppm <cesarta.po@gmail.com>
2023-04-10 17:09:38 -0600

    final del capitulo 21

--- lib/src/pages/widgets/headers.dart
+++ lib/src/pages/widgets/headers.dart
@@ -116,11 +116,159 @@ class _HeaderTriangularPainter extends CustomPainter{
 
     //Dibujar con el path y el lapiz
     //path.moveTo(0, size.height * 0.50);
-    path.lineTo(size.width, size.height * 0.50);
-    path.lineTo(size.width, 0);
-    path.lineTo(0, 0);
-    path.lineTo(0, size.height * 0.5);
+    path.lineTo(size.width, size.height);
+    path.lineTo( 0, size.height);
+    //path.lineTo(0, 0);
+    //path.lineTo(0, size.height * 0.5);
 
+
+
+    canvas.drawPath(path, lapiz);
+
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    throw UnimplementedError();
+  }
+
+}
+
+
+class HeaderPico extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+    color:Color.fromARGB(255, 80, 11, 107),
+    child: CustomPaint(
+      painter: _HeaderTriangularPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderPicoPainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+
+    final lapiz = Paint();
+
+    //Propiedades
+    lapiz.color = Color(0xff615AAB);
+    lapiz.style = PaintingStyle.fill; //.fill
+    //lapiz.style = PaintingStyle.fill;  //Rellena el color del cuadro dibujado
+    lapiz.strokeWidth = 20;
+
+    final path = new Path();
+
+    path.lineTo( 0 , size.height * 0.25);
+    path.lineTo(size.width * 0.5, size.height *0.25);
+    path.lineTo(size.width, size.height * 0.20);
+    path.lineTo(size.width, 0);
+
+
+    canvas.drawPath(path, lapiz);
+
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    throw UnimplementedError();
+  }
+
+}
+
+class HeaderCurvo extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+    color:Color.fromARGB(255, 80, 11, 107),
+    child: CustomPaint(
+      painter: _HeaderCurvoPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderCurvoPainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+
+    final lapiz = Paint();
+
+    //Propiedades
+    lapiz.color = Color(0xff615AAB);
+    lapiz.style = PaintingStyle.fill; //.fill
+    //lapiz.style = PaintingStyle.fill;  //Rellena el color del cuadro dibujado
+    lapiz.strokeWidth = 20;
+
+    final path = new Path();
+
+    path.lineTo( 0 , size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.50, size.width * 0.4, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+    //path.lineTo(size.width, size.height * 0.25);
+
+
+    canvas.drawPath(path, lapiz);
+
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    throw UnimplementedError();
+  }
+
+}
+
+class HeaderWave extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+    color:Color.fromARGB(255, 80, 11, 107),
+    child: CustomPaint(
+      painter: _HeaderWavePainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderWavePainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+
+    final lapiz = Paint();
+
+    //Propiedades
+    lapiz.color = Color(0xff615AAB);
+    lapiz.style = PaintingStyle.fill; //.fill
+    //lapiz.style = PaintingStyle.fill;  //Rellena el color del cuadro dibujado
+    lapiz.strokeWidth = 20;
+
+    final path = new Path();
+
+    path.lineTo( 0 , size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.50, size.width * 0.4, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+    //path.lineTo(size.width, size.height * 0.25);
 
 
     canvas.drawPath(path, lapiz);
